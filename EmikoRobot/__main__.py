83fb471e4e655552ed67b638758ef3caf936ec92
projecttex <104363696+projecttex@users.noreply.github.com>
2022-05-08 03:46:14 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -431,7 +431,7 @@ def emiko_about_callback(update, context):
         )
     elif query.data == "emiko_support":
         query.message.edit_text(
-            text="*๏ Emiko support chats*"
+            text="*• ᴀʙᴏᴜᴛ ᴠᴀʀ sᴜᴘᴘᴏʀᴛ*"
             f"\nJoin My Support Group/Channel for see or report a problem on {dispatcher.bot.first_name}.",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
